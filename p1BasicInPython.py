7798dc94fee6908c89a441916086055ca2247be7
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-12-25 19:59:08 +0530

    Add files via upload

---
+++ p1BasicInPython.py
@@ -0,0 +1,32 @@
+# print function
+print("SIMPLE PRINT FUNCTION\n")
+print("Name","Sneha",sep=":",end="\nOkay thank you\n\n")
+
+#data types and variables
+print("DATATYPES AND VARIABLES\n")
+a="sneha"
+b=90
+c=True
+d=None
+e=90.9
+print(b+e)
+print(a,"=",type(a))
+print(b,"=",type(b))
+print(c,"=",type(c))
+print(d,"=",type(d))
+print(e,"=",type(e),end="\n\n")
+
+#Lists
+print("LIST:A list is an ordered collection of data with elements separated by a comma and enclosed within square brackets.Lits are mutable(that can be change) \n")
+list1=["sneha",90,90.99,True,["apple","banana"]]
+print(list1,end="\n\n")
+
+#Tuple
+print("TUPLE:A tuple is an ordered collection of data with elements separated by a comma and enclosed wihin parentheses.Tuples are imutable(cannot be change)\n")
+tuple1=("sneha",(9.89,90),("abc","cba"))
+print(tuple1,end="\n\n")
+
+#dictionary
+print("DICTIONARY:A dictionary is an unordered collection of  data containing a key:value pair . The key:value pair is enclosed within curly brackets\n")
+dict1={"name":"sneha","age":19,"gender":"female"}
+print(dict1,end="\n\n")
