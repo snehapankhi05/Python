6519b48a284b7b4dc2934812c90cf3558f2864a0
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-12-27 14:47:53 +0530

    Add files via upload

---
+++ p3StringsInPython.py
@@ -0,0 +1,17 @@
+"""
+STRINGS
+ In python,anything that you enclose between single or double quotaion marks is considered a string.
+ A string is essentially a sequence or array of textual data.
+ Strings are used when working with Unicode characters.
+"""
+
+name="sneha"
+address="""
+b16/f220 model township,
+surat
+"""
+age='hello "my age is 19" '
+
+print("Hello"+ name)
+print(address)
+print(age)
