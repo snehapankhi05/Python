2d000a307c8c620cc4a05f55d7addafd3a34e3b2
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-12-28 16:21:59 +0530

    Add files via upload

---
+++ p4StringSlicing.py
@@ -0,0 +1,6 @@
+fruit="mango"
+len1=len(fruit)
+print("The size of fruit", fruit , "is" , len1)
+print(fruit[0:3])
+print(fruit[-4:-1])
+# - means len(fruit)- the number i.e -4:-1 -> len(fruit)-4 : len(fruit)-1 i.e  1:4-->> ang
