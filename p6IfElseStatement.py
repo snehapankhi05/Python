ed11aebbc38640f9724a07315247ef33e053d07d
SNEHA PANKHI <snehapankhi105@gmail.com>
2025-01-03 16:26:59 +0530

    Add files via upload

---
+++ p6IfElseStatement.py
@@ -0,0 +1,13 @@
+"""
+IF-ELSE STATEMENTS
+Sometimes the programmer needs to check the evaluation of certain expression(s),whether the expression evaluate to True
+or False.If the expression evaluates to False,then the prgram exceution follows a different path than it would have if
+the expression had evaluated to True.
+"""
+age=int(input("Please enter your age: "))
+if (age>18):
+    print("You are elgible to vote")
+
+else:
+    print("You are not elgible to vote")
+
