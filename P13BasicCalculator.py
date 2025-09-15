72892de3a49588ab26a3baffca31f73c20fe2142
SNEHA PANKHI <Techsneha19@gmail.com>
2025-09-15 13:34:50 +0530

    Add files via upload

---
+++ P13BasicCalculator.py
@@ -0,0 +1,22 @@
+"""
+Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+"""
+
+num1=float(input("Please enter first number "))
+num2=float(input("Please enter second number "))
+
+print("Addition: ")
+print(num1+num2)
+print("Subtraction: ")
+print(num1-num2)
+print("Multiplication: ")
+print(num1*num2)
+print("Division: ")
+print(num1/num2)
